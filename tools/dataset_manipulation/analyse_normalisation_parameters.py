d36498d5a5180db679e985bb0b58264f6fd890f6
anmakon <anna.konrad.2020@mumail.ie>
2020-07-15 07:40:32 +0100

    Added mode to apply normalisation parameters to data

--- tools/dataset_manipulation/analyse_normalisation_parameters.py
+++ tools/dataset_manipulation/analyse_normalisation_parameters.py
@@ -1,37 +1,104 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import argparse
 import csv
 
 class Analyse_Normalisation():
-	def __init__(self):
+	def __init__(self,calc,visu):
 		cornell_path = "./data/training/Cornell/"
-		self.cornell = True
 		self.output_path = "./analysis/Normalisation/"
 		if not os.path.exists(self.output_path):
 			os.mkdir(self.output_path)
 		dexnet_path = "./data/training/dexnet_2_tensor/"
+
 		self.num_random_files = 10000
-		self._compute_mean_and_std(cornell_path)
-		self.cornell = False
-#		self.robust = 0.002
-		self._compute_mean_and_std(dexnet_path)
+
+		if calc:
+			self.cornell = True
+			print("Cornell.")
+			self._compute_mean_and_std(cornell_path)
+			self.cornell = False
+			print("DexNet.")
+			self._compute_mean_and_std(dexnet_path)
+		if visu:
+			self.cornell = True
+			self._visualise_normalised_data(cornell_path)
+			self.cornell = False
+			self._visualise_normalised_data(dexnet_path)
 		return None
 
-	def _compute_mean_and_std(self,path):
-		self.im_mean = 0
-		self.im_std = 0
+	def _visualise_normalised_data(self,path):
+		random_file_indices = self._get_random_file_indices(path)
+		self._get_normalisation_data('Cornell')
+		self._normalise_depth_and_pose(path,random_file_indices)
+		#plot histograms
+		self._plt_single_hist(self.depths,'image',norm_data='Cornell')
+		self._plt_single_hist(self.pos,'pos',norm_data='Cornell')
+
+		self._get_normalisation_data('DexNet')
+		self._normalise_depth_and_pose(path,random_file_indices)
+		#plot histograms
+		self._plt_single_hist(self.depths,'image',norm_data='DexNet')
+		self._plt_single_hist(self.pos,'pos',norm_data='DexNet')
+
+	def _normalise_depth_and_pose(self,path,random_files):
 		self.depths = []
-		self.pos_depths = []
-		self.neg_depths = []
-		self.mean_pos = 0
-		self.mean_neg = 0
-		self.std_pos = 0
-		self.std_neg = 0
+		self.pos = []
+		for tensor,array in random_files:
+			im_data,pos = self._load_training_data(path,int(tensor),int(array))
+			self.pos.append((pos-self.pos_mean)/self.pos_std)
+			data = ((im_data-self.im_mean)/self.im_std).flatten().tolist()
+			self.depths.extend(data)
+		return None
+
+
+	def _get_normalisation_data(self,data):
+		self.im_mean = np.load(self.output_path+data+'_image_mean.npy')
+		self.im_std = np.load(self.output_path+data+'_image_std.npy')
+		self.pos_mean = np.load(self.output_path+data+'_pos_mean.npy')
+		self.pos_std = np.load(self.output_path+data+'_pos_std.npy')
+		return None
+
+
+	def _compute_mean(self,path,random_files):
+		num_im = 0
 		num_pos = 0
-		num_neg = 0
-		num_summed = 0
+		for tensor,array in random_files:
+			im_data,pos = self._load_training_data(path,int(tensor),int(array))
+			num_im += im_data.shape[0]*im_data.shape[1]*im_data.shape[2]
+			num_pos += 1
+			self.im_mean += np.sum(im_data)
+			self.pos_mean += pos
+		self.im_mean = self.im_mean / num_im
+		self.pos_mean = self.pos_mean / num_pos
+		return None
+
+	def _compute_std(self,path,random_files):
+		num_im = 0
 		outliers = []
+		num_pos = 0
+		for tensor,array in random_files:
+			im_data,pos = self._load_training_data(path,int(tensor),int(array))
+			data = im_data.flatten().tolist()
+			num_im += im_data.shape[0]*im_data.shape[1]*im_data.shape[2]
+			num_pos += 1
+			self.depths.extend(data)
+			self.pos.append(pos)
+			if min(data)< 0.2:
+				print("Found depth image with outlier.")
+				print("Dataset:",path.split('/')[-1])
+				print("Tensor: ", tensor, "; Array: ",array)
+				outliers.append([tensor,array,tensor,array])
+			self.im_std += np.sum((im_data - self.im_mean)**2)
+			self.pos_std += np.sum((pos - self.pos_mean)**2)
+		self.im_std = np.sqrt(self.im_std / num_im)
+		self.pos_std = np.sqrt(self.pos_std / num_pos)
+		if len(outliers) >= 1:
+			self._save_outlier_pointers(outliers)
+		return 1
+	
+	def _get_random_file_indices(self,path):
 		index = self._read_training_indices(path)
 		num_random_files = min(self.num_random_files,len(index))
 		random_index = np.random.choice(index,
@@ -42,45 +109,25 @@ class Analyse_Normalisation():
 			random_file_indices = [[pos//500,pos%500] for pos in random_index]
 		else:
 			random_file_indices = [[pos//1000,pos%1000] for pos in random_index]
+		return random_file_indices
 
-		for tensor,array in random_file_indices:
-			im_data,lab = self._load_training_data(path,int(tensor),int(array))
-			num = im_data.shape[0]*im_data.shape[1]*im_data.shape[2]
-			self.im_mean += np.sum(im_data)
-			if lab == 1:
-				self.mean_pos += np.sum(im_data)
-				num_pos += num
-			else:
-				self.mean_neg += np.sum(im_data)
-				num_neg += num
-			num_summed += num
-		self.im_mean = self.im_mean / num_summed
-		self.mean_pos = self.mean_pos / num_pos
-		self.mean_neg = self.mean_neg / num_neg
+	def _compute_mean_and_std(self,path):
+		self.im_mean = 0
+		self.im_std = 0
+		self.pos_mean = 0
+		self.pos_std = 0
+		self.pos = []
+		self.depths = []
+
+		random_file_indices = self._get_random_file_indices(path)
+		print("Amount of files: ",len(random_file_indices))
+		self._compute_mean(path,random_file_indices)
 		print("Mean is:",self.im_mean)
-		for tensor,array in random_file_indices:
-			im_data, lab = self._load_training_data(path,int(tensor),int(array))
-			self.im_std += np.sum((im_data - self.im_mean)**2)
-			data = im_data.flatten().tolist()
-			if lab == 1:
-				self.std_pos += np.sum((im_data-self.mean_pos)**2)
-				self.pos_depths.extend(data)
-			else:
-				self.std_neg += np.sum((im_data-self.mean_neg)**2)
-				self.neg_depths.extend(data)
-			if min(data)< 0.2:
-				print("Found depth image with outlier.")
-				print("Dataset:",path.split('/')[-1])
-				print("Tensor: ", tensor, "; Array: ",array)
-				outliers.append([tensor,array,tensor,array])
-			self.depths.extend(data)
-		self.im_std = np.sqrt(self.im_std / num_summed)	
-		self.std_pos = np.sqrt(self.std_pos / num_pos)
-		self.std_neg = np.sqrt(self.std_neg / num_neg)
+		self._compute_std(path,random_file_indices)
+
 		print("Std is:",self.im_std)
-		self._plt_histograms()
-		if len(outliers) >= 1:
-			self._save_outlier_pointers(outliers)
+		self.save_data()
+
 	
 	def _save_outlier_pointers(self,outliers):
 		export_path = "./data/training/csv_files"
@@ -94,54 +141,82 @@ class Analyse_Normalisation():
 				writer.writerow(row)
 		return None
 
-	def _plt_histograms(self):
-		self._plt_single_hist(self.im_mean,self.im_std,self.depths,"")
-		self._plt_single_hist(self.mean_pos,self.std_pos,self.pos_depths,"_positive")
-		self._plt_single_hist(self.mean_neg,self.std_neg,self.neg_depths,"_negative")
+	def save_data(self):
+		self._plt_single_hist(self.depths,'image',mean=self.im_mean,std=self.im_std)
+		self._plt_single_hist(self.pos,'pos',mean=self.pos_mean,std=self.pos_std)
 
-
-	def _plt_single_hist(self,mean,std,depths,mode):
+	def _plt_single_hist(self,data,mode,mean=None,std=None,norm_data=None):
 		if self.cornell:
-			data = 'Cornell'
+			dset = 'Cornell'
 			unit = '[m]'
 		else:
-			data= 'DexNet'
+			dset= 'DexNet'
 			unit = '[units]'
-		path = self.output_path + "Depths_in_"+data+mode
+		if mode == 'image':
+			data_type = 'Depth'
+		elif mode == 'pos':
+			data_type = 'Pose'
+		path = self.output_path + data_type+"s_in_"+dset+'_'+mode
+		if mean is None and std is None:
+			path += '_normalised_on_'+norm_data
+			binwidth = 0.05
+			title = data_type+" values in "+dset+" dataset normalised on "+norm_data+" data"
+			unit = ', normalised'
+		else:
+			binwidth = 0.001
+			title = data_type+" values in "+dset+ " dataset"
+
+			#Save Mean and Std to .npy files
+			np.save(self.output_path+dset+'_'+mode+"_mean",mean)
+			np.save(self.output_path+dset+'_'+mode+"_std",std)
 		
-		binwidth = 0.001
-		n,bins,_ =plt.hist(depths,bins = np.arange(min(depths),max(depths)+binwidth,binwidth))
-		plt.title("Depth values in "+data+" " + mode[1:])
-		plt.xlabel("Depth from Camera "+unit)
-		plt.vlines(mean,0,max(n),colors='r')
-		plt.text(bins[1],max(n)*7/8,"Mean: "+str(round(mean,3)))
-		plt.vlines(mean-std,0,max(n),colors='r')
-		plt.vlines(mean+std,0,max(n),colors='r')	
-		plt.text(bins[1],max(n)*6/8,"Std: "+str(round(std,3)))
-		plt.savefig(path+"_all")	
-		plt.text(mean-2.5*std,max(n)*7/8,"Mean: "+str(round(mean,3)))
-		plt.text(mean-2.5*std,max(n)*6/8,"Std: "+str(round(std,3)))
-		plt.xlim((mean-3*std,mean+3*std))
+		# Create and save histogram of Depths/Poses
+		n,bins,_ =plt.hist(data,bins = np.arange(min(data),max(data)+binwidth,binwidth))
+		plt.title(title)
+		plt.xlabel(data_type+" "+unit)
+		if mean is not None and std is not None:
+			plt.vlines(mean,0,max(n),colors='r')
+			plt.text(bins[1],max(n)*7/8,"Mean: "+str(round(mean,3)))
+			plt.vlines(mean-std,0,max(n),colors='r')
+			plt.vlines(mean+std,0,max(n),colors='r')	
+			plt.text(bins[1],max(n)*6/8,"Std: "+str(round(std,3)))
+		plt.savefig(path+"_all")
+		# Save a 2nd image with a limited x-axis	
+		if mean is not None and std is not None:
+			plt.text(mean-2.5*std,max(n)*7/8,"Mean: "+str(round(mean,3)))
+			plt.text(mean-2.5*std,max(n)*6/8,"Std: "+str(round(std,3)))
+			plt.xlim((mean-3*std,mean+3*std))
+		else:
+			plt.xlim((-1.5,1.5))
 		plt.savefig(path)	
 		plt.close()
 		return None
 
 
 	def _load_training_data(self,path,tensor,array):
-		robust = 0.002
 		depth_path = path +"tensors/depth_ims_tf_table_"
-		label_path = path +"tensors/robust_ferrari_canny_"
-		robustness = np.load(label_path+("{0:05d}").format(tensor)+".npz")['arr_0'][array]
-		if robustness >= robust:
-			label = 1
-		else:
-			label = 0
+		label_path = path +"tensors/hand_poses_"
+		hand_pose = np.load(label_path+("{0:05d}").format(tensor)+".npz")['arr_0'][array]
 		depth = np.load(depth_path+("{0:05d}").format(tensor)+".npz")['arr_0'][array]
-		return depth,label
+		return depth,hand_pose[2]
 
 	def _read_training_indices(self,path):
 		path += "splits/image_wise/train_indices.npz"
 		return np.load(path)['arr_0'] 
 	
 if __name__ == "__main__":
-	Analyse_Normalisation()
+	parser = argparse.ArgumentParser(description=("Analyse the normalisation parameters of DexNet and Cornell"))
+	parser.add_argument("--calc",
+				type = bool,
+				default = False,
+				help="Calculate the mean and std")
+	parser.add_argument("--visu",
+				type = bool,
+				default = False,
+				help="Visualise the normalised depths/poses")
+
+	args = parser.parse_args()
+	calc = args.calc
+	visu = args.visu
+				
+	Analyse_Normalisation(calc,visu)
